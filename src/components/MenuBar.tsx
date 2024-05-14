ec5da4cca9434a2b67435291571b275a5934db92
leo-exee <leopold@littlebill.io>
2024-05-14 21:01:20 +0200

    chore: Remove unused code and update AppBar in MenuBar component

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,20 +1,12 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, IconButton, Toolbar } from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LoginIcon from "@mui/icons-material/Login";
-import { useNavigate } from "react-router-dom";
 import { getUserToken } from "../constants/api.constant";
 
 const MenuBar = () => {
-  const navigate = useNavigate();
-  const handleHome = () => {
-    navigate("/");
-  };
   return (
     <AppBar position="static">
       <Toolbar variant="dense" className="flex justify-between items-center">
-        <Typography variant="h6" onClick={handleHome}>
-          My Trone
-        </Typography>
         <IconButton
           edge="end"
           color="inherit"
